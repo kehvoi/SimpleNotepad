f8cffefd31f35b4c95d1faded513a3f3f4ef626f
Kehvoi Thompson <kehvoithompson@gmail.com>
2025-12-08 17:24:20 -0500

    Added the ability to read files

--- src/Functionality.java
+++ src/Functionality.java
@@ -1,8 +1,15 @@
+import javax.swing.*;
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class Functionality {
 
     GUI gui;
+    String fileName;
+    String fileAddress;
+
+
     public Functionality(GUI gui){
 
         this.gui = gui;
@@ -19,7 +26,23 @@ public class Functionality {
         fileDia.setVisible(true);
 
         if(fileDia.getFile() !=null){
+            fileName = fileDia.getFile();
+            fileAddress = fileDia.getDirectory();
+            gui.window.setTitle(fileName);
 
+        }
+
+        try {
+            BufferedReader bRead = new BufferedReader(new FileReader(fileAddress + fileName));
+            gui.textArea.setText("");
+            String line = null;
+            while((line = bRead.readLine())!=null){
+                gui.textArea.append(line + "\n");
+            }
+            bRead.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            JOptionPane.showMessageDialog(null, "File not opened", "Open Error", JOptionPane.ERROR_MESSAGE);
         }
     }
 }
